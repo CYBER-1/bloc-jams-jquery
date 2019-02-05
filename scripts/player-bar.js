193bbd831059aee8fe40fca14290688da57ceb2a
Mykola Mykhailian <mnykolay@gmail.com>
2019-02-05 13:37:28 -0800

    previous button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,5 +14,19 @@
        const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
       });
-   }
+
+      $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+         
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+         
+    // not sure where nextSongIndex comes from, not sure if previousSongIndex is correct //     
+        const previousSongIndex = currentSongIndex - 1;
+        if (previousSongIndex >= album.songs.length) { return; }
+         
+        const previousSong = album.songs[previousSongIndex];
+         player.playPause(previousSong);
+       });
+    }
+   
    
